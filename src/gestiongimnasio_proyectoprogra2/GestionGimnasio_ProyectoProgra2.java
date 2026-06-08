60863797dccee0695f0b2c01039c92ba6e738d3d
naza <nazareth.rodriguez@uamcr.net>
2026-06-07 20:29:51 -0600

    Primer commit

---
+++ src/gestiongimnasio_proyectoprogra2/GestionGimnasio_ProyectoProgra2.java
@@ -0,0 +1,10 @@
+
+package gestiongimnasio_proyectoprogra2;
+
+public class GestionGimnasio_ProyectoProgra2 {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
