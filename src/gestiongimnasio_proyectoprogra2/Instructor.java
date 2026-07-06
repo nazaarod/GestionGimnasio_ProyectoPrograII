6312ce9f4448d78bf424798540ec5beca5dff5f9
fabri <fabri@Fabricio.lan>
2026-07-05 21:58:05 -0600

    Mis cambios

--- src/gestiongimnasio_proyectoprogra2/Instructor.java
+++ src/gestiongimnasio_proyectoprogra2/Instructor.java
@@ -1,5 +1,9 @@
 package gestiongimnasio_proyectoprogra2;
 
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+
 public class Instructor extends Persona {
 
     private String especialidad;
@@ -22,6 +26,35 @@ public class Instructor extends Persona {
     @Override
     public void mostrarResumen() {
 
+        
+        // 1. Crear la ventana con un titulo
+        JFrame ventana = new JFrame("Mostrar Resumen de Prueba");
+        ventana.setSize(400, 200);
+        ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        ventana.setLocationRelativeTo(null); 
+
+        
+        String texto = "<html><div style='text-align: center;'>"
+                     + "<h1>Instructor</h1>"
+                     + "Identificacion: "+identificacion+"<br>"
+                     + "Nombre Completo: "+nombreCompleto+"<br>"
+                     + "Telefono: "+telefono+"<br>"
+                     + "Edad: "+edad+"<br>"
+                     + "correo: "+correo+"<br>"
+                     + "</div></html>";
+
+        // 3. Crear la etiqueta que sostiene el texto y centrarlo
+        JLabel etiqueta = new JLabel(texto, SwingConstants.CENTER);
+
+        // 4. Meter el texto directo a la ventana
+       ventana.add(etiqueta);
+
+        // 5. Hacerla visible
+        ventana.setVisible(true);
+        
+        
+        
+        
         System.out.println("--INSTRUCTOR--");
         System.out.println("Identificacion: " + identificacion);
         System.out.println("Instructor: " + nombreCompleto);
